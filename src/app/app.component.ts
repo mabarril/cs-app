bae2b3b3d44cb6754eb4cdad6de5582d07e63f5e
mabarril <mabarril@gmail.com>
2024-05-07 14:14:12 +0000

    chore: Update app.component.html and app.component.ts to use ToolbarComponent instead of Home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from "./pages/home/home.component";
+import { ToolbarComponent } from "./components/toolbar/toolbar.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HttpClientModule, HomeComponent]
+    imports: [RouterOutlet, HttpClientModule, HomeComponent, ToolbarComponent]
 })
 export class AppComponent {
   title = 'cs-app';
