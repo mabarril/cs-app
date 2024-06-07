f3701366cdbc6631f40e86f0e44c0de98e401bb9
Marcelo Alexandre Barrionuevo <mabarril@gmail.com>
2024-06-07 10:52:08 -0300

    api

--- src/app/pages/pagamento/controle-pagamento/controle-pagamento.component.ts
+++ src/app/pages/pagamento/controle-pagamento/controle-pagamento.component.ts
@@ -6,20 +6,22 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatCardModule } from '@angular/material/card';
-import { ResponsavelService } from '../../../services/responsavel.service';
-import { Responsavel } from '../../../models/responsavel.model';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatListModule } from '@angular/material/list';
-import { RegistroService } from '../../../services/registro.services';
-import { Registro } from '../../../models/registro.model';
 import { MatDialog } from '@angular/material/dialog';
-import { UserDialogComponent } from '../../../components/user-dialog/user-dialog.component';
-import { ItemPago } from '../../../models/itemPago.model';
-import { RecebimentoRegistro } from '../../../models/recebimento_registro';
-import { ControleRecebimentoService } from '../../../services/controle-recebimento.service';
 import { MatIconModule } from '@angular/material/icon';
 import { CurrencyPipe } from '@angular/common';
+
+import { UserDialogComponent } from '../../../components/user-dialog/user-dialog.component';
+
+import { RecebimentoRegistro } from '../../../models/recebimento_registro';
+import { Responsavel } from '../../../models/responsavel.model';
 import { Recebimento } from '../../../models/recebimento.model';
+import { ItemPago } from '../../../models/itemPago.model';
+import { Registro } from '../../../models/registro.model';
+
+import { ResponsavelService } from '../../../services/responsavel.service';
+import { ControleRecebimentoService } from '../../../services/controle-recebimento.service';
 
 @Component({
   selector: 'app-controle-pagamento',
@@ -42,6 +44,7 @@ export class ControlePagamentoComponent implements OnInit {
 
   dialogRef: any;
 
+  formApto = false
 
   responsaveis?: Responsavel[];
   selectedResponsavel: Responsavel | undefined;
@@ -56,18 +59,15 @@ export class ControlePagamentoComponent implements OnInit {
 
   constructor(private responsavelService: ResponsavelService, public dialog: MatDialog, private controleRecebimentoService: ControleRecebimentoService) { }
 
+
+
   ngOnInit(): void {
     this.responsavelService.getAll().subscribe(responsaveis => {
       this.responsaveis = responsaveis
     });
   }
 
-  // $responsavel = $data["responsavel"];
-  // $dtPgto = $data["dtPgto"];
-  // $forma = $data["forma"];
-  // $descricao = $data["descricao"];
-  // $recibo = $data["recibo"];
-  // $itens = $data["itens"];
+
 
   private fb = inject(FormBuilder);
   controlePagamentoForm = this.fb.group({
@@ -75,7 +75,9 @@ export class ControlePagamentoComponent implements OnInit {
     valor: [null, Validators.required],
     data: [null, Validators.required],
     formaPagamento: [null, Validators.required],
-    descricao: [null, Validators.required]
+    descricao: null, // não obrigatório
+    recibo: [null, Validators.pattern('^[0-9]*$')]
+
   });
 
   onSubmit(): void {
@@ -83,16 +85,23 @@ export class ControlePagamentoComponent implements OnInit {
     let recebimento: Recebimento = {
       responsavel: this.selectedResponsavel?.nome_responsavel || '',
       dtPgto: (this.controlePagamentoForm.value.data as unknown as Date)?.toISOString().split('T')[0]
-      || new Date().toISOString().split('T')[0],
+        || new Date().toISOString().split('T')[0],
       descricao: this.controlePagamentoForm.value.descricao || '',
       forma: this.selectedFormaPagamento || '',
       itens: this.listaItensPago
     };
-  
+
     this.controleRecebimentoService.create(recebimento).subscribe(recebimento => {
       console.log(recebimento);
     });
-  
+  }
+
+  validateForm(): void {
+    if (this.selectedResponsavel && this.selectedItem && this.selectedFormaPagamento && this.listaItensPago.length > 0, this.controlePagamentoForm.valid) {
+      this.formApto = true;
+    } else {
+      this.formApto = false;
+    };
   }
 
   openUserDialog() {
@@ -110,7 +119,5 @@ export class ControlePagamentoComponent implements OnInit {
     this.totalPago = this.totalPago + itemPago.valor;
     console.log(this.listaItensPago);
   }
-
-
 }
 
