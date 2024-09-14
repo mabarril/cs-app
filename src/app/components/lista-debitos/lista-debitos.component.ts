26e724f062db33b7fe0de5c0c0781ad2955010a5
Marcelo Alexandre Barrionuevo <mabarril@gmail.com>
2024-09-14 12:01:57 -0300

    refactor: Update CSS styles for payment and debit components

--- src/app/components/lista-debitos/lista-debitos.component.ts
+++ src/app/components/lista-debitos/lista-debitos.component.ts
@@ -1,49 +1,51 @@
 import { Component, Inject, OnInit, ViewChild } from '@angular/core';
-import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 import { DebitoService } from '../../services/debito.service';
 import { Debito } from '../../models/debito.model';
 import { CurrencyPipe, DatePipe, NgIf } from '@angular/common';
 import { MatDialogRef, MAT_DIALOG_DATA, MatDialogClose } from '@angular/material/dialog';
 
 
+export interface Dvb {
+  id: number;
+  nome: string;
+}
+
 @Component({
   selector: 'app-lista-debitos',
   standalone: true,
-  imports: [NgIf, DatePipe, CurrencyPipe, MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule],
+  imports: [NgIf, DatePipe, CurrencyPipe, MatFormFieldModule, MatInputModule, MatTableModule, MatButtonModule],
   templateUrl: './lista-debitos.component.html',
   styleUrl: './lista-debitos.component.css'
 })
 export class ListaDebitosComponent implements OnInit {
 
-  displayedColumns: string[] = ['id', 'valor', 'vencimento', 'descricao'];
+  displayedColumns: string[] = ['valor', 'vencimento', 'descricao'];
   dataSource!: MatTableDataSource<Debito>;
   clickedRows = new Set<Debito>();
   totalDebito: number = 0;
-
-  @ViewChild(MatSort) sort!: MatSort;
-
+  dbv: Dvb;
 
   constructor(
     private debitoService: DebitoService,
-
     public dialogRef: MatDialogRef<ListaDebitosComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-  ) { };
+  ) { this.dbv = data };
 
   ngOnInit(): void {
     console.log(this.clickedRows);
-    this.debitoService.getDebitoDesbravador(1473577).subscribe((data) => {
+    this.debitoService.getDebitoDesbravador(this.dbv.id).subscribe((data) => {
       this.dataSource = new MatTableDataSource(data);
     });
   }
 
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
+  // applyFilter(event: Event) {
+  //   const filterValue = (event.target as HTMLInputElement).value;
+  //   this.dataSource.filter = filterValue.trim().toLowerCase();
+  // }
 
   selectRow(row: Debito) {
     this.clickedRows.has(row) ? this.clickedRows.delete(row) : this.clickedRows.add(row);
--- src/app/components/pagamento/pagamento.component.ts
+++ src/app/components/pagamento/pagamento.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
 import { AsyncPipe, CurrencyPipe, DatePipe } from '@angular/common';
@@ -12,6 +12,16 @@ import { Registro } from '../../models/registro.model';
 import { RegistroService } from '../../services/registro.services';
 import { RecebimentoService } from '../../services/recebimento.service';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatIcon } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
+import { MatNativeDateModule, provideNativeDateAdapter } from '@angular/material/core';
+import { Responsavel } from '../../models/responsavel.model';
+import { RegistraResponsavelComponent } from '../registra-responsavel/registra-responsavel.component';
+import { ResponsavelService } from '../../services/responsavel.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ListaDebitosComponent } from '../lista-debitos/lista-debitos.component';
+
 
 
 export interface RelacaoDebitos {
@@ -36,7 +46,11 @@ const ELEMENT_DATA: RelacaoDebitos[] = [{ nome: 'João', item: 'Item 1', vlrDevi
     MatButtonModule,
     MatTableModule,
     CurrencyPipe,
+    MatIcon,
+    MatDatepickerModule,
+    MatSelectModule
   ],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './pagamento.component.html',
   styleUrl: './pagamento.component.css'
 })
@@ -46,12 +60,36 @@ export class PagamentoComponent {
   dataToDisplay = [...ELEMENT_DATA];
   dataSource = new MatTableDataSource<RelacaoDebitos>(this.dataToDisplay);
 
+  selectedFormaPagamento: string | undefined;
+  formaPagamento: string[] = ['Pix', '7me', 'Dinheiro', 'Cartão'];
+
+  selectedItem: string | undefined;
+  itens: string[] = ['Mensalidade', 'Eventos', 'Uniforme'];
+
+  responsaveis?: Responsavel[];
+  selectedResponsavel: Responsavel | undefined;
+  filteredOptionsResp!: Observable<Responsavel[]>;
+  dialogRef: any;
+
+  data = {} as any;
+
   constructor(
-    private registroService: RegistroService, private recebimentoService: RecebimentoService,) { };
+    private registroService: RegistroService, private recebimentoService: RecebimentoService, private responsavelService: ResponsavelService, public dialog: MatDialog,) { };
 
   myControl = new FormControl<string | Registro>('');
   options!: Registro[];
   filteredOptions!: Observable<Registro[]>;
+
+  private fb = inject(FormBuilder);
+  controlePagamentoForm = this.fb.group({
+    responsavel: [null, Validators.required],
+    valor: [null, Validators.required],
+    data: [null, Validators.required],
+    formaPagamento: [null, Validators.required],
+    descricao: null, // não obrigatório
+    recibo: [null, Validators.pattern('^[0-9]*$')],
+    item: [null, Validators.required]
+  });
 
 
   ngOnInit(): void {
@@ -84,11 +122,41 @@ export class PagamentoComponent {
   }
 
   selecionaRegistro(registro: Registro) {
-    console.log('8888888  ', registro.id);
+    
+    this.data  = { nome : registro.nome, id: registro.id};
     this.recebimentoService.getExtrato(registro.id!).subscribe(result => {
       // this.extrato = result;
       // this.calculatePaymentsTotal();
     });
+  }
+
+  onResponsavelSelection(resp: Responsavel) {
+    this.selectedResponsavel = resp;
+    console.log(this.selectedResponsavel);
+  }
+
+  openResponsavelDialog() {
+    let data = {} as Responsavel;
+    this.dialogRef = this.dialog.open(RegistraResponsavelComponent,
+      { data: data, height: 'auto', width: '480px', autoFocus: true });
+
+    this.dialogRef.afterClosed().subscribe((result: Responsavel) => {
+      result.nome_responsavel = result.nome_responsavel!.toUpperCase();
+      this.responsaveis?.push(result);
+      this.responsavelService.create(result).subscribe((res: Responsavel) => {
+        console.log(res);
+      });
+    });
+  }
+
+  openListaDebitosDialog() {
+    this.dialogRef = this.dialog.open(ListaDebitosComponent,
+      { data: this.data, height: 'auto', width: '480px', autoFocus: true });
+
+    // this.dialogRef.afterClosed().subscribe((result: ItemPago) => {
+    //   this.adicionarItem(result);
+    // });
+
   }
 
 }
